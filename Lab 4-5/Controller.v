02b87cd6252e24b93faec3793ee2e5d82c93ef9e
Willyrains <willyrains424@gmail.com>
2023-10-21 17:28:25 -0700

    Update Controller.v

--- Lab 4-5/Controller.v
+++ Lab 4-5/Controller.v
@@ -41,17 +41,34 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             //To get ALU control value for each instruction
             case(Instruction[5:0])
 
-                6'b000000: begin                        //sll
+                6'b000000: begin //sll
                     ALUControl <= 5'b00100;
+                    RegWrite <= 0;
+                    ALUSrc <= 0;
+                    RegDst <= 0;
+                    MemWrite <= 2'b00;
+                    MemRead <= 2'b00;
+                    Branch <= 0;
+                    MemToReg <= 0;
+                    Jump <= 0;
+                    Jr <= 0;
+                    Jal <= 0;
                     ShiftControl <= 1;
+                
                 end
-                6'b000010: begin                        //srl
-                    ALUControl <= 5'b00101;
+                6'b000010: begin
+                    ALUSrc <= 0;
+                    ALUControl <= 5'b00101;      //srl
                     ShiftControl <= 1;
+                
                 end
-                6'b100000: ALUControl <= 5'b00001;      //add
+                6'b100000: begin 
+                ALUControl <= 5'b00001;
+                ALUSrc <= 0;
+                ShiftControl <= 0; 
+                end
                 6'b100010: ALUControl <= 5'b00010;      //sub
-                6'b011100: ALUControl <= 5'b00011;      //mul
+                6'b011000: ALUControl <= 5'b00011;      //mult
                 6'b100100: ALUControl <= 5'b00110;      //and
                 6'b100101: ALUControl <= 5'b00111;      //or
                 6'b100110: ALUControl <= 5'b01000;      //xor
@@ -71,6 +88,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b00001;      //add to get address
+            ShiftControl <= 0;
         end
         6'b100000: begin                                //lb
             RegWrite <= 1;
@@ -84,6 +102,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b00001;      //add to get address
+            ShiftControl <= 0; 
         end
         6'b100001: begin                                //lh
             RegWrite <= 1;
@@ -97,6 +116,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b00001;      //add to get address
+            ShiftControl <= 0;
         end      
         6'b101011: begin                            //sw
             RegWrite <= 0;
@@ -110,6 +130,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b00001;     //add to get address
+            ShiftControl <= 0;
         end
         6'b101000: begin                            //sb
             RegWrite <= 0;
@@ -123,6 +144,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b00001;     //add to get address
+            ShiftControl <= 0;
         end
         6'b101001: begin                            //sh
             RegWrite <= 0;
@@ -136,6 +158,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b00001;     //add to get address
+            ShiftControl <= 0;
         end
         6'b001000: begin            //addi
             RegWrite <= 1;
@@ -149,6 +172,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b00001;
+            ShiftControl <= 0;
         end 
         6'b001100: begin            //andi
             RegWrite <= 1;
@@ -162,6 +186,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b00110;
+            ShiftControl <= 0;
         end 
         6'b001101: begin            //ori
             RegWrite <= 1;
@@ -175,6 +200,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b00111;
+            ShiftControl <= 0;
         end
         6'b001110: begin            //xori
             RegWrite <= 1;
@@ -188,6 +214,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b01000;
+            ShiftControl <= 0;
         end
         6'b001010: begin            //slti
             RegWrite <= 1;
@@ -201,6 +228,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b01110;
+            ShiftControl <= 0;
         end
         6'b000101: begin            //bne
             RegWrite <= 0;
@@ -214,6 +242,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b01111;
+            ShiftControl <= 0;
         end
         6'b000100: begin            //beq
             RegWrite <= 0;
@@ -227,6 +256,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b01100;
+            ShiftControl <= 0;
         end
         6'b000001: begin            //bgez, bltz
             RegWrite <= 0;
@@ -239,6 +269,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jump <= 0;
             Jr <= 0;
             Jal <= 0;
+            ShiftControl <= 0;
             case(Instruction[20:16])            //Uses rt as an extension of the opcode
                 5'b00001: begin                 //bgez
                     ALUControl <= 5'b01111;
@@ -260,6 +291,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b10000;
+            ShiftControl <= 0;
         end
         6'b000110: begin            //blez
             RegWrite <= 0;
@@ -273,6 +305,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b10001;
+            ShiftControl <= 0;
         end
         6'b000010: begin            //j - Add or gate for what comes out of the AND gate and jump
             RegWrite <= 0;
@@ -286,6 +319,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'bxxxxx;
+            ShiftControl <= 0;
         end
         6'b000011: begin            //jal
             RegWrite <= 0;
@@ -299,6 +333,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 1;
             ALUControl <= 5'bxxxxx;
+            ShiftControl <= 0;
         end
         6'b001001: begin            //jr
             RegWrite <= 0;
@@ -312,6 +347,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 1;
             Jal <= 0;
             ALUControl <= 5'bxxxxx;
+            ShiftControl <= 0;
         end
         default: begin
             RegWrite <= 0;
@@ -325,6 +361,7 @@ module Controller(Instruction, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Bran
             Jr <= 0;
             Jal <= 0;
             ALUControl <= 5'b00000;
+            ShiftControl <= 0;
         end
         endcase
     end    
