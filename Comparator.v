f6cd4e818d3cda16be84284aa4b7e21c90c87ded
Samantha Perry <samnperry@arizona.edu>
2023-11-01 15:07:30 -0700

    Update Comparator.v

--- Comparator.v
+++ Comparator.v
@@ -5,7 +5,7 @@
 // 
 // Create Date: 10/16/2023 12:53:58 PM
 // Design Name: 
-// Module Name: Fetch_to_Decode
+// Module Name: Comparator
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,3 +20,17 @@
 // Percent Effort: Kari Cordes - 33%, William Rains - 33%, Samantha Perry - 33%
 // 
 //////////////////////////////////////////////////////////////////////////////////
+
+module Comparator(A, B, A_grt_B, A_ls_B, A_eq_B);
+  input [3:0] A, B,
+  output reg A_grt_B, A_ls_B, A_eq_B;
+  
+  always@(*) begin
+    A_grt_B = 0; A_ls_B = 0; A_eq_B = 0;
+    if(A>B) A_grt_B = 1'b1;
+    else if(A<B) A_less_B = 1'b1;
+    else A_eq_B = 1'b1;
+  end
+
+
+endmodule
