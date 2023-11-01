bc29f39633d539d1f921a4f595998ff6f18181f4
jadelynx912 <karicordes@arizona.edu>
2023-11-01 14:52:01 -0700

    Created files

---
+++ Comparator.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/16/2023 12:53:58 PM
+// Design Name: 
+// Module Name: Fetch_to_Decode
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+// Percent Effort: Kari Cordes - 33%, William Rains - 33%, Samantha Perry - 33%
+// 
+//////////////////////////////////////////////////////////////////////////////////
