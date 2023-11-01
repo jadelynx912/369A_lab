48b6370f0950793bffc11debffc1d6072f4ca4d9
jadelynx912 <karicordes@arizona.edu>
2023-11-01 15:13:03 -0700

    Update HazardDetection.v

--- HazardDetection.v
+++ HazardDetection.v
@@ -20,3 +20,21 @@
 // Percent Effort: Kari Cordes - 33%, William Rains - 33%, Samantha Perry - 33%
 // 
 //////////////////////////////////////////////////////////////////////////////////
+
+module HazardDetection(rs, rt, rtExecution, MemReadExecution, DecodeRegWrite, PCWrite, MuxControl);
+    input [4:0] rs, rt, rtExecution;
+    input MemReadExecution;
+    output reg DecodeRegWrite, PCWrite, MuxControl;
+    
+    if (MemReadExecution & ((rt == rtExecution) | (rs == rtExecution))) begin
+        PCWrite <= 0;
+        DecodeRegWrite <= 0;
+        MuxControl <= 1;
+    end
+    else begin
+        PCWrite <= 1;
+        DecodeRegWrite <= 0;
+        MuxControl <= 0;
+    end
+
+endmodule
