dfbce20c11c5962d0bd1bff35380c38b553fd134
jadelynx912 <karicordes@arizona.edu>
2023-11-06 16:54:22 -0800

    fixed top level

--- Components/ControlMux.v
+++ Components/ControlMux.v
@@ -20,41 +20,47 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module ControlMux(RegWrite, PreRegWrite, ALUSrc, PreALUSrc, RegDst, PreRegDst, MemWrite, PreMemWrite, MemRead, PreMemRead, Branch, PreBranch, MemToReg, MemToReg, Jump, PreJump, Jr, PreJr, Jal, PreJal, ALUControl, PreALUControl, ShiftControl, PreShiftControl);
+module ControlMux(PreRegWrite, PreALUSrc, PreRegDst, PreMemWrite, PreMemRead, PreMemToReg, PreJump, PreJr, PreJal, PreALUControl, PreShiftControl, PrePCSrc,
+                    RegWrite, ALUSrc, RegDst, MemWrite, MemRead, MemToReg, Jump, Jr, Jal, ALUControl, ShiftControl, PCSrc, controlMuxSignal);
+
 
 input controlMuxSignal;
+input PreRegWrite, PreALUSrc,PreRegDst, PreMemToReg, PreJump, PreJr, PreJal, PreShiftControl, PrePCSrc;
+input [1:0] PreMemWrite, PreMemRead;
+input [4:0] PreALUControl;
+
 output reg RegWrite, ALUSrc, RegDst, MemToReg, Jump, Jr, Jal, ShiftControl, PCSrc;
 output reg [4:0] ALUControl;
 output reg [1:0] MemWrite, MemRead;
 
 always @(*) begin
     if (controlMuxSignal == 1) begin
-    RegWrite <= PreRegWrite;
-    ALUSrc <= PreALUSrc;
-    RegDst <= PreRegDst;
-    MemWrite <= PreMemWrite;
-    MemRead <= PreMemRead;
-    Branch <= PreBranch;
-    MemToReg <= MemToReg;
-    Jump <= PreJump;
-    Jr <= PreJr;
-    Jal <= PreJal;
-    ALUControl <= PreALUControl;
-    ShiftControl <= PreShiftControl;
+        RegWrite <= PreRegWrite;
+        ALUSrc <= PreALUSrc;
+        RegDst <= PreRegDst;
+        MemWrite <= PreMemWrite;
+        MemRead <= PreMemRead;
+        MemToReg <= MemToReg;
+        Jump <= PreJump;
+        Jr <= PreJr;
+        Jal <= PreJal;
+        ALUControl <= PreALUControl;
+        ShiftControl <= PreShiftControl;
+        PCSrc <= PrePCSrc;
     end
     else if (controlMuxSignal == 0) begin
-    RegWrite <= 0;
-    ALUSrc <= 0;
-    RegDst <= 0;
-    MemWrite <= 0;
-    MemRead <= 0;
-    Branch <= 0;
-    MemToReg <= 0
-    Jump <= 0;
-    Jr <= 0;
-    Jal <= 0;
-    ALUControl <= 0;
-    ShiftControl <= 0;
+        RegWrite <= 0;
+        ALUSrc <= 0;
+        RegDst <= 0;
+        MemWrite <= 0;
+        MemRead <= 0;
+        MemToReg <= 0;
+        Jump <= 0;
+        Jr <= 0;
+        Jal <= 0;
+        ALUControl <= 0;
+        ShiftControl <= 0;
+        PCSrc <= 0;
     end
 end
 
--- Components/top_level.v
+++ Components/top_level.v
@@ -70,11 +70,10 @@ wire [31:0] ReadData, MemReadDataWrite, ALUResultWrite, PCAddResultWrite;
 wire [4:0] RegRdWrite;
 
 
-//assign PCSrc_Jump_OR = JumpMemory | PCSrc; //Do we grab Jump straight from controller or from memory pipeline?
 
-Mux32Bit2To1 PCountSrc(PCSrcOutput, PCAdder_SignExtensionMemory, PCAddResult, PCSrc); 
+Mux32Bit2To1 PCountSrc(PCSrcOutput, PCAdder_SignExtension, PCAddResult, PCSrc); 
 
-Mux32Bit2To1 JRMux(Jump_To_PC, ReadData1Execute, PCSrcOutput, JrExecute); 
+Mux32Bit2To1 JRMux(Jump_To_PC, ReadData1, PCSrcOutput, Jr); 
 
 Mux32Bit2To1 JumpMux(NextPC, jOffset, Jump_To_PC, Jump);
 
@@ -86,7 +85,7 @@ PCAdder adder(PCResult, PCAddResult);
 
 
 //INSTRUCTION FETCH STAGE / DECODE STAGE
-Fetch_To_Decode ftd(PCAddResult, instruction,  PCAddResultDecode, instructionDecode, Clk, Reset);
+Fetch_To_Decode ftd(PCAddResult, instruction,  PCAddResultDecode, instructionDecode, Clk, Reset, DecodeWrite);
 
 RegisterFile reggy(instructionDecode[25:21], instructionDecode[20:16], RegRdWrite, WriteDataReg, RegWriteWrite, Clk, ReadData1, ReadData2);
 
@@ -104,45 +103,27 @@ Mux5bit2to1 regDest(regDstMux, instructionDecode[15:11], instructionDecode[20:16
 
 Mux5bit2to1 JalRAMux(regDstOutput, 5'b11111, regDstMux, Jal); //$ra is reg 31
 
-Controller controlly(instructionDecode, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Branch, MemToReg, Jump, Jr, Jal, ALUControl, ShiftControl);
-
-//Lab 6 work
-
-//TODO: JalRAMux regDstOutput not wired
-      //naming inconsistency in rdExecute
-
-Mux5bit2to1 Mux1(RegWrite, PreRegWrite, 1'b0, MuxControl);
-Mux5bit2to1 Mux2(ALUSrc, PreALUSrc, 1'b0, MuxControl);
-Mux5bit2to1 Mux3(RegDst, PreRegDst, 1'b0, MuxControl);
-Mux5bit2to1 Mux4(MemWrite, PreMemWrite, 1'b0, MuxControl);
-Mux5bit2to1 Mux5(MemRead, PreMemRead, 1'b0, MuxControl);
-Mux5bit2to1 Mux6(Branch, PreBranch, 1'b0, MuxControl);
-Mux5bit2to1 Mux7(MemToReg, PreMemToReg, 1'b0, MuxControl);
-Mux5bit2to1 Mux8(Jump, PreJump, 1'b0, MuxControl);
-Mux5bit2to1 Mux9(Jr, JrPreJr, 1'b0, MuxControl);
-Mux5bit2to1 Mux10(Jal, PreJal, 1'b0, MuxControl);
-Mux5bit2to1 Mux11(ALUControl, PreALUControl, 1'b0, MuxControl);
-Mux5bit2to1 Mux12(ShiftControl, PreShiftControl, 1'b0, MuxControl);
+Controller controlly(instructionDecode, A_grt_B, A_ls_B, A_eq_B, PreRegWrite, PreALUSrc, PreRegDst, PreMemWrite, PreMemRead, PreMemToReg, PreJump, PreJr, PreJal, PreALUControl, PreShiftControl, PrePCSrc);
+//Controller(Instruction, gt, lt, eq, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, MemToReg, Jump, Jr, Jal, ALUControl, ShiftControl, PCSrc);
 
 Comparator compy (ReadData1, ReadData2, A_grt_B, A_ls_B, A_eq_B);
+
+ControlMux controlMux1(PreRegWrite, PreALUSrc, PreRegDst, PreMemWrite, PreMemRead, PreMemToReg, PreJump, PreJr, PreJal, PreALUControl, PreShiftControl, PrePCSrc,
+                        RegWrite, ALUSrc, RegDst, MemWrite, MemRead, MemToReg, Jump, Jr, Jal, ALUControl, ShiftControl, PCSrc, controlMuxSignal);
+//ControlMux(PreRegWrite, PreALUSrc, PreRegDst, PreMemWrite, PreMemRead, PreMemToReg, PreJump, PreJr, PreJal, PreALUControl, PreShiftControl, PrePCSrc,
+//                    RegWrite, ALUSrc, RegDst, MemWrite, MemRead, MemToReg, Jump, Jr, Jal, ALUControl, ShiftControl, PCSrc, controlMuxSignal);
+
 
 HazardDetection hazzy (instructionDecode, A_grt_B, A_ls_B, A_eq_B, RegDstExecute, RdMemory, DecodeRegWrite, PCWrite, MuxControl, RegWriteExecute, RegWriteMemory);
 
 
 //EXECUTE STAGE
-Decode_To_Execute dte (Clk, Reset, DecodeRegWrite, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, MemToReg, Jr, Jal, ALUControl, PCAddResultDecode, ShiftSwitchWire, ReadData2, signExtend,
-                                   RegWriteExecute, ALUSrcExecute, RegDstExecute, MemWriteExecute, MemReadExecute, MemToRegExecute, JrExecute, JalExecute, ALUControlExecute, PCAddResultExecute, ReadData1Execute, ReadData2Execute, SignExtExecute);
-
-//modulDecodeo_Execute(Clk, Reset, RegWrite, ALUSrc, RegDst, MemWrite, MemRead, Branch, MemToReg, Jr, Jal, ALUControl, PCAddResult, ReadData1, ReadData2, SignExt, RegDst1, RegDst2, 
-//                                 RegWriteOut, ALUSrcOut, RegDstOut, MemWriteOut, MemReadOut, BranchOut, MemToRegOut, JrOut, JalOut, ALUControlOut, PCAddResultOut, ReadData1Out, ReadData2Out, SignExtOut, RegDst1Out, RegDst2Out);
-//BRANCH, RegDst1, RegDst2, BranchOut, RegDst1Out, RegDst2Out
-
-//REMOVE REGDST EXECUTE FROM REGISTER FILE (Does it even need to be passed through)
-//WILL START HERE AND GO UP 
+Decode_To_Execute dte (Clk, Reset, RegWrite, ALUSrc, MemWrite, MemRead, MemToReg, Jal, ALUControl, PCAddResultDecode, ShiftSwitchWire, ReadData2, signExtend, regDstOutput,
+    RegWriteExecute, ALUSrcExecute, MemWriteExecute, MemReadExecute, MemToRegExecute, JalExecute, ALUControlExecute, PCAddResultExecute, ReadData1Execute, ReadData2Execute, SignExtExecute, rdExecute);
 
 Mux32Bit2To1 aluSource(ALUSrcOutput, SignExtExecute, ReadData2Execute, ALUSrcExecute); //SignExtOut if 1, ReadData2Out if 0
 
-ALU32Bit alu(ALUControlExecute, ReadData1Execute, ALUSrcOutput, ALUResult, Zero);
+ALU32Bit alu(ALUControlExecute, ReadData1Execute, ALUSrcOutput, ALUResult);
 
 
 //DATA MEMORY STAGE
